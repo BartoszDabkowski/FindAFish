97587c1f4cf37659a9cc0d13f044b66759f54976
Bartosz Dabkowski (C BDabkows I 0657) <BDabkows@us.pinkertons.com>
2017-10-14 14:31:33 -0700

    added font-awesome library

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -21,6 +21,7 @@ const nonTreeShakableModules = [
     'es6-shim',
     'event-source-polyfill',
     'jquery',
+    'font-awesome/scss/font-awesome.scss'
 ];
 const allModules = treeShakableModules.concat(nonTreeShakableModules);
 
