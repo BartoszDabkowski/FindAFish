01f61d126f1d8ada615448005df914d4c547c04e
Bartosz Dabkowski (C BDabkows I 0657) <BDabkows@us.pinkertons.com>
2017-10-13 18:20:43 -0700

    bootstrap 4-beta upgrade

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -26,7 +26,7 @@ const allModules = treeShakableModules.concat(nonTreeShakableModules);
 
 module.exports = (env) => {
     const extractSass = new ExtractTextPlugin({
-        filename: "[name].[contenthash].css",
+        filename: "[name].css", //"[name].[contenthash].css",
         disable: process.env.NODE_ENV === "development"
     });
 
@@ -93,9 +93,21 @@ module.exports = (env) => {
             libraryTarget: 'commonjs2',
         },
         module: {
-            rules: [ { test: /\.css(\?|$)/, use: ['to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] } ]
+            rules: [{
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            }]
         },
         plugins: [
+            extractSass,
             new webpack.DllPlugin({
                 path: path.join(__dirname, 'ClientApp', 'dist', '[name]-manifest.json'),
                 name: '[name]_[hash]'
