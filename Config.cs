5b4548894025c41a92870a8fc5fb3d9aefc6d85d
Bartosz Dabkowski (C BDabkows I 0657) <BDabkows@us.pinkertons.com>
2017-10-17 11:05:17 -0700

    added identityserver4 configuration and testing setup

---
+++ Config.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace FindAFish {
+    public static class Config {
+        public static List<TestUser> GetUsers() => new List<TestUser>{
+            new TestUser(){
+                SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                Username = "John",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim("first_name", "John"),
+                    new Claim("last_name", "Doe")
+                }
+            },
+            new TestUser(){
+                SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                Username = "Sally",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim("first_name", "Sally"),
+                    new Claim("last_name", "Mae")
+                }
+            }
+        };
+        public static IEnumerable<IdentityResource> GetIdentityResources() => new List<IdentityResource>{
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<Client> GetClients(){
+            return new List<Client>();
+        } 
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace FindAFish
 {
@@ -22,14 +23,22 @@ namespace FindAFish
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients());
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) 
         {
             if (env.IsDevelopment())
             {
+                loggerFactory.AddConsole();
+                loggerFactory.AddDebug();
                 app.UseDeveloperExceptionPage();
                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
                 {
@@ -42,6 +51,8 @@ namespace FindAFish
             }
 
             app.UseStaticFiles();
+
+            app.UseIdentityServer();
 
             app.UseMvc(routes =>
             {
